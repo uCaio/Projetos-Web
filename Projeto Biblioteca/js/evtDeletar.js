00b181fdc39e4c6c53565be559517546cf3ae6d8
Caio Gonçalves <145069681+uCaio@users.noreply.github.com>
2025-03-09 09:52:05 -0300

    Delete Projeto Biblioteca directory

--- Projeto Biblioteca/js/evtDeletar.js
+++
@@ -1,9 +0,0 @@
-let evtDeletar = () => {
-    let btnsDeletar = document.querySelectorAll('.btn-Deleta');
-    btnsDeletar.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            let nomeLivro = e.target.getAttribute('data-nome');
-            deletarLivro(nomeLivro);
-        });
-    });
-};
